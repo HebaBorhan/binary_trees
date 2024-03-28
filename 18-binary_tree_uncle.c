a20ce4c5a4b0ebe224cf88b80ede0b97af9743d0
Heba <hebamagde@gmail.com>
2024-03-28 12:02:13 +0200

    update uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,12 +10,12 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 binary_tree_t *grandparent = node->parent->parent;
 if (node == NULL || node->parent == NULL ||
-node->parent->parent == NULL)
+grandparent == NULL)
 {
 return (NULL);
 }
 if (grandparent->left ==  node->parent)
-return (grandparent->right);
+return (&grandparent->right);
 else
-return (grandparent->left);
+return (&grandparent->left);
 }
